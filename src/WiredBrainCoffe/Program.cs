93ca7d6e05218494890206552cb461cc44cedba8
Ale <45163764+Sudo-Ale@users.noreply.github.com>
2022-05-01 13:01:36 +0200

    Initial Commit
    Wired Brain Coffe Survey

---
+++ src/WiredBrainCoffe/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WiredBrainCoffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculated Values
+            var responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
+            var unanswaredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
+            var overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
+
+            Console.WriteLine($"Response Percentage: {responseRate}");
+            Console.WriteLine($"Unanswared Surveys: {unanswaredCount}");
+            Console.WriteLine($"Overall score: {overallScore}");
+
+            //Logical Comparisons
+            var higherCoffeeScore = Q1Results.CoffeeScore > Q1Results.FoodScore;
+            var customersRecommend = Q1Results.WouldRecommend >= 7;
+            var noGranolaYesCapuccino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino";
+
+            Console.WriteLine($"Coffee Score Higher Than Food: {higherCoffeeScore}");
+            Console.WriteLine($"Customers Would Recommend Us: {customersRecommend}");
+            Console.WriteLine($"Hate Granola, Love Capuccino: {noGranolaYesCapuccino}");
+        }
+    }
+}
