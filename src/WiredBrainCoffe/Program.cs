f7a56cb1dd354cb4457112f588c65ddcdcecafd1
Ale <45163764+Sudo-Ale@users.noreply.github.com>
2022-05-01 13:19:27 +0200

    Response rate tasks
    Added list of rate task.

--- src/WiredBrainCoffe/Program.cs
+++ src/WiredBrainCoffe/Program.cs
@@ -6,23 +6,24 @@ namespace WiredBrainCoffe
     {
         static void Main(string[] args)
         {
+            var tasks = new List<string>();
             //Calculated Values
             var responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
-            var unanswaredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
             var overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
 
-            Console.WriteLine($"Response Percentage: {responseRate}");
-            Console.WriteLine($"Unanswared Surveys: {unanswaredCount}");
-            Console.WriteLine($"Overall score: {overallScore}");
+            if(Q1Results.CoffeeScore < Q1Results.FoodScore)
+            {
+                tasks.Add("Investigate coffee recipes and ingredients");
+            }
 
-            //Logical Comparisons
-            var higherCoffeeScore = Q1Results.CoffeeScore > Q1Results.FoodScore;
-            var customersRecommend = Q1Results.WouldRecommend >= 7;
-            var noGranolaYesCapuccino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino";
-
-            Console.WriteLine($"Coffee Score Higher Than Food: {higherCoffeeScore}");
-            Console.WriteLine($"Customers Would Recommend Us: {customersRecommend}");
-            Console.WriteLine($"Hate Granola, Love Capuccino: {noGranolaYesCapuccino}");
+            if(overallScore > 8.0)
+            {
+                tasks.Add("Work with leadership to reward staff");
+            }
+            else
+            {
+                tasks.Add("Work with employees for improvement ideas");
+            }
         }
     }
 }
